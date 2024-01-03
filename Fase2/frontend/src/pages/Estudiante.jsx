38de571025530a2180be129fe90470d6cd1a9795
RoniRoland <roni0293@gmail.com>
2024-01-03 11:34:49 -0600

    ver cursos estudiante

--- Fase2/frontend/src/pages/Estudiante.jsx
+++ Fase2/frontend/src/pages/Estudiante.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/otros.css'
 
@@ -19,6 +19,55 @@ function Estudiante() {
         e.preventDefault();
         window.open("/", "_self");
     };
+    const [cursos, setCursos] = useState([]);
+
+    useEffect(() => {
+        async function PedirCursos() {
+            const valorLocal = localStorage.getItem("user");
+            const response = await fetch("http://localhost:4000/obtener-clases", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    Carnet: valorLocal,
+                }),
+            });
+            const result = await response.json();
+            console.log(result);
+            setCursos(result.Arreglo);
+        }
+        PedirCursos();
+    }, []);
+
+    const Palabra = () => {
+        return (
+            <div className="row">
+                <div className="row align-items-start">
+                    {cursos.map((item, i) => (
+                        <div className="form-signin1 col" key={"CursoEstudiante" + i}>
+                            <div className="text-center">
+                                <div className="card card-body">
+                                    <h1 className="text-left" key={"album" + i} value={i}>
+                                        {item}
+                                    </h1>
+                                    <div>
+                                        <span
+                                            className="input-group-text"
+                                            id="validationTooltipUsernamePrepend"
+                                        ></span>{" "}
+                                        <br />
+                                    </div>
+                                </div>
+                                <br />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    };
+
     return (
         <div class="px-4 pt-5 my-5 text-center border-bottom">
             <h1 className="display-4 fw-bold text-body-emphasis bienvenido-titulo">Bienvenido Estudiante</h1>
@@ -40,6 +89,10 @@ function Estudiante() {
                     <br />
                 </div>
                 <br />
+                <br />
+                <h2 className="display-5 fw-bold text-body-emphasis bienvenido-titulo">Cursos Asignados</h2>
+                <br />
+                {cursos.length > 0 ? <Palabra /> : null}
                 <br />
             </div>
         </div>
